c291dfd8c3b635d0e3124140a2b3d7bef4babf5c
juan jose <jlemus003@gmail.com>
2018-07-25 21:04:47 -0600

    Modulo suma

---
+++ suma.java
@@ -0,0 +1,28 @@
+public class Suma{
+
+  private double num1;
+  private double num2;
+
+  public Suma(){
+  }
+
+  public double getNum1(){
+      return this.num1;
+  }
+
+  public void setNum1(double num1){
+     this.num1=num1;
+  }
+
+  public double getNum2(){
+      return this.num1;
+  }
+
+  public void setNum1(double num1){
+     this.num1=num2;
+  }
+
+  public void sumarNumeros(){
+         
+  }
+}
